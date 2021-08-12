ea86b6c4e106f56a36f70757fe509b290ce6774c
Ben <ben@reticent-monlith.com>
2021-08-12 10:13:38 +0100

    proxies to backend and broker

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
-from quart import Quart
+from quart import Quart, render_template
 from quart_cors import cors
 
 app = Quart(__name__)
 app = cors(app, allow_origin="*")
 
-@app.route("/api")
-def api_root():
-    return "Hello"
+@app.route("/api/hello")
+async def api_root():
+    return await render_template("hello.html")
